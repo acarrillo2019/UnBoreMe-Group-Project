328b38e0a7437ba7e9bdc38192308dcbe54405b8
acarrillo2019 <alric21@aol.com>
2020-01-25 11:04:03 -0700

    Adjusted query URL

--- assets/app.js
+++ assets/app.js
@@ -15,11 +15,19 @@ $(document).ready(function () {
             database = firebase.database()
 
 
+            var searchTerm = $("#searchTerm").val().trim();
+            var category = $("#category").val().trim();
+            var state = $("#state").val().trim();
+            var city = $("#city").val().trim();
+            var radius = $("#radius").val().trim();
+
+
+
             // These variables hold will feed data to the querlyURL. The variable values will come from user input. Currently some of the variables hold temporary data for testing purposes.
-            var search = "";
-            var where = "";
-            var radius = "";
-            var category = "";
+            var searchTerm = $(this).attr("data-name");
+            var where = $(this).attr("data-name");
+            var radius = $(this).attr("data-name");
+            var category = $(this).attr("data-name");
             var when = "future";
             var results = "25"
             var key = "vMvwtd4qCcNr8hZL";
@@ -29,13 +37,13 @@ $(document).ready(function () {
 
 
 
+            var queryURL = "http://api.eventful.com/json/events/search?app_key=" + key + "&q=" + searchTerm + "&l=" + where + "&within=" + radius + "&t=" + when + "&c=" + category + "&page_size=" + results + "&oauth_consumer_key=" + oAuthKey + "&oauth_signature_method=HMAC-SHA1&oauth_version=1.0&scheme=https";
 
 
 
             function searchEvents(search) {
                 // queryURL to pull data from Eventful.com. The proxy URL is necessary to circumvent CORS rejection.
-                var queryURL = "http://api.eventful.com/json/events/search?app_key=" + key + "&q=" + search + "&l=" + where + "&within=" + radius + "&t=" + when + "&c=" + category + "&page_size=" + results + "&oauth_consumer_key=" + oAuthKey + "&oauth_signature_method=HMAC-SHA1&oauth_version=1.0&scheme=https";
-
+              
                 $.ajax({
                     url: proxyURL + queryURL,
                     method: "GET",
@@ -54,6 +62,26 @@ $(document).ready(function () {
                         var lng = temp.events.event[i].longitude;
                     }
 
+                    // first the entire response must be parsed from its JSON origin
+                    temp = JSON.parse(response)
+                    // then we can grab the important event information from the nest
+                    eventData = temp.events.event
+                    console.log(eventData)
+                    for (var i = 0; i < eventData.length; i++) {
+                        newCountry = $("<p>")
+                        newCountry.text(eventData[i].country_name)
+                        newCity = $("<p>")
+                        newCity.text(eventData[i].city_name)
+                        newTime = $("<p>")
+                        newTime.text(eventData[i].start_time)
+                        newTitle = $("<p>")
+                        newTitle.text(eventData[i].title)
+                        newEvent = $("<div>")
+                        newEvent.append(newCountry, newCity, newTime, newTitle)
+                        $("#resultCard").append(newEvent)
+                    }
+
+
                 });
 
             };
@@ -69,10 +97,10 @@ $(document).ready(function () {
             var state = $("#state").val().trim();
             var city = $("#city").val().trim();
             var radius = $("#radius").val().trim();
-
+            var where = stae + city;
 
             // Running the searchEvents function(passing search queries as arguments)
-            searchEvents(searchTerm, category, state, city, radius);
+            searchEvents(searchTerm, category, where, radius);
         })
 
 
